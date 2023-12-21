a2bc4c29ff2ea31f62df5d1d116ce542143bbf80
Richard Giles <richard.giles@gmail.com>
2023-12-21 11:12:51 +0800

    .

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-      <h1>Hello</h1>
+    <h1>Last 7 Days Activity Data from Oura Ring</h1>
+    <div id="activityData"></div>
+    <canvas id="myChart" width="800" height="800"></canvas>
+    <!-- <script src="/script.js"></script> -->
   )
 }
