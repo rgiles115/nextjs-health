94008b767eec4e10c1472809f00c6cdf59df8c81
Richard Giles <richard.giles@gmail.com>
2023-12-21 11:27:57 +0800

    Attempt to add ActivityChart to the app

---
+++ app/components/ActivityChart.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect, useRef } from 'react';
+import Chart from 'chart.js/auto';
+
+const ActivityChart = () => {
+    const [activityData, setActivityData] = useState({ dates: [], activeCalories: [] });
+    const chartRef = useRef(null);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/api/getActivityData')  // Replace with your server API endpoint
+            .then(response => response.json())
+            .then(data => {
+                const dates = data.data.map(entry => entry.day);
+                const activeCalories = data.data.map(entry => entry.active_calories);
+                setActivityData({ dates, activeCalories });
+            })
+            .catch(error => console.error('Error:', error));
+    }, []);
+
+    useEffect(() => {
+        if (activityData.dates.length > 0) {
+            const ctx = chartRef.current.getContext('2d');
+            const gradientFill = ctx.createLinearGradient(0, 0, 0, ctx.canvas.clientHeight);
+            gradientFill.addColorStop(0, 'rgba(138, 43, 226, 0.6)');
+            gradientFill.addColorStop(1, 'rgba(138, 43, 226, 0)');
+
+            new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: activityData.dates,
+                    datasets: [{
+                        label: 'Active Calories',
+                        data: activityData.activeCalories,
+                        fill: true,
+                        backgroundColor: gradientFill,
+                        borderColor: 'rgba(138, 43, 226, 1)',
+                        pointBackgroundColor: 'rgba(138, 43, 226, 1)',
+                        borderWidth: 2,
+                        lineTension: 0.3
+                    }]
+                },
+                options: {
+                    // ...your options here
+                }
+            });
+        }
+    }, [activityData]);
+
+    return (
+        <canvas ref={chartRef} width="800" height="800"></canvas>
+    );
+};
+
+export default ActivityChart;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
-import Image from 'next/image'
+import Head from 'next/head';
+import ActivityChart from '../components/ActivityChart';
 
 export default function Home() {
   return (
-    <h1>Last 7 Days Activity Data from Oura Ring</h1>
+    <div>
+      <Head>
+        <title>Oura Ring Activity Data</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="/style.css" />
+      </Head>
+      <h1>Last 7 Days Activity Data from Oura Ring</h1>
+      <div id="activityData"></div>
+      <ActivityChart />
+    </div>
   )
 }
