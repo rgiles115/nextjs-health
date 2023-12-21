64260af21042ffecc013d22631dd058fd6e58d30
Richard Giles <richard.giles@gmail.com>
2023-12-21 11:12:41 +0800

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Oura',
+  title: 'Oura Ring Data',
   description: 'an app to diaply Oura data',
 }
 
