d12c7631d438170ee3787e9516c32fbaf02139ce
Richard Giles <richard.giles@gmail.com>
2024-01-10 16:33:27 +0800

    Added SpeedInights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Inter } from 'next/font/google'
 import './globals.css'
 
@@ -16,7 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}
+      <SpeedInsights />
+      </body>
     </html>
   )
 }
